08dc0a69922a65f2d34acab0a2f47c6581b40058
abd-elqader <abdelqader.montaser@gmail.com>
2024-05-14 12:38:03 +0300

    don't repeat one quote in next click on button

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
-
-
-console.log('abood welcome');
-
-
-
-
+var pastIndex;
 
 function changeIndex() {
     quotes = 
         [
-            '“Be yourself; everyone else is already taken.”',
-            '“So many books, so little time.”',
-            '“A room without books is like a body without a soul.”',
-            '“You only live once, but if you do it right, once is enough.”',
-            "“If you tell the truth, you don't have to remember anything.”",
-            "“A friend is someone who knows all about you and still loves you.”"
+            {
+                name: "Oscar Wilde",
+                quote: "“Be yourself; everyone else is already taken.”",
+            },
+            {
+                name: "Frank Zappa",
+                quote: "“So many books, so little time.”",
+            },
+            {
+                name: "Marcus Tullius Cicero",
+                quote: "“A room without books is like a body without a soul.”",
+            },
+            {
+                name: "Mae West",
+                quote: "“You only live once, but if you do it right, once is enough.”",
+            },
+            {
+                name: "Mark Twain",
+                quote: "“If you tell the truth, you don't have to remember anything.”",
+            },
+            {
+                name: "Elbert Hubbard",
+                quote: "“A friend is someone who knows all about you and still loves you.”"
+            },
         ];
-    var index = Math.floor(Math.random() * quotes.length);
-    console.log(index);
-    x = document.getElementById('quote').innerHTML 
-    console.log(x);
-    // quotes[index];    
+
+    var index;
+
+    // this for don't repeat one quote in next click on button
+    do{
+        index = Math.floor(Math.random() * quotes.length);
+    }while( index == pastIndex);
+    
+    document.getElementById('quote').innerHTML = quotes[index].quote; 
+    document.getElementById('name').innerHTML = quotes[index].name; 
+    pastIndex = index;
 }
 
